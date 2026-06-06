1541ca1d71e6dd4aa73318a4dcf09515c4b240f9
Cesar Capelli <avillacapelli@gmail.com>
2026-06-06 08:58:30 -0300

    feat: adiona formulário interativo para registro de gastos

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print("Apenas para iniciar o git comitando o arquivo app.py.....")
+import streamlit as st
+import pandas as pd
+
+if "gastos" not in st.session_state:
+    st.session_state.gastos = pd.DataFrame(columns=["Data", "Categoria", "Descrição", "Valor"])
+
+# BARRA LATERAL
+st.sidebar.header("Adicionar Novo Gasto")
+with st.sidebar.form("form_adicionar", clear_on_submit=True):
+    data = st.date_input("Data")
+    categoria = st.selectbox("Categoria",["Alimentação", "Transporte", "Moradia", "Lazer", "Saúde", "Outros"])
+    descricao = st.text_input("Descrição")
+    valor = st.number_input("Valor (R$)", min_value=0.0, format="%.2f")
+
+    botao_adicionar = st.form_submit_button("Adicionar")
+
+if botao_adicionar:
+    novo_gasto = pd.DataFrame([{
+        "Data": data,
+        "Categoria": categoria,
+        "Descrição": descricao,
+        "Valor": valor
+    }])
+    st.session_state.gastos = pd.concat([st.session_state.gastos, novo_gasto], ignore_index=True)
+
+st.title("Meu App Financeiro")
+st.dataframe(st.session_state.gastos)
