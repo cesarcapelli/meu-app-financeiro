cccf63be70715971b409740315165eff2cf48df3
Cesar Capelli <avillacapelli@gmail.com>
2026-06-08 08:52:36 -0300

    feat: adiciona metrica de total de gastos e notificacao toast de gasto adicionado com suceso

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ if "gastos" not in st.session_state:
 st.sidebar.header("Adicionar Novo Gasto")
 with st.sidebar.form("form_adicionar", clear_on_submit=True):
     data = st.date_input("Data")
-    categoria = st.selectbox("Categoria",["Alimentação", "Transporte", "Moradia", "Lazer", "Saúde", "Outros"])
+    categoria = st.selectbox("Categoria", ["Alimentação", "Transporte", "Moradia", "Lazer", "Saúde", "Outros"])
     descricao = st.text_input("Descrição")
     valor = st.number_input("Valor (R$)", min_value=0.0, format="%.2f")
 
@@ -22,6 +22,12 @@ if botao_adicionar:
         "Valor": valor
     }])
     st.session_state.gastos = pd.concat([st.session_state.gastos, novo_gasto], ignore_index=True)
+    st.toast("Gasto adicionado com sucesso!", icon="✅")
 
-st.title("Meu App Financeiro")
+st.title("My Grana")
+
+#CALCULANDO E EXIBINDO O TOTAL DE GASTOS
+total_gastos = st.session_state.gastos["Valor"].sum()
+st.metric(label="Total de Gastos", value=f"R$ {total_gastos:.2f}")
+
 st.dataframe(st.session_state.gastos)
